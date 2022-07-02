ee450a488856c9697d18ffd37dc53878fb091963
Stefan Schlicker <stefanschlicker@posteo.de>
2022-07-02 13:26:12 +0200

    boolean state isPlaying - hide game and hide end button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,29 @@
 import Player from "../components/Player/Player.js";
 import AddPlayer from "../components/Player/AddPlayer.js";
 import Button from "../components/Button/Button";
+import { useState } from "react";
 
 export default function HomePage({ players }) {
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const endGameHandler = () => {
+    setIsPlaying(false);
+  };
+
+  const displayGame = players.map((player) => {
+    return <Player key={player.id} name={player.name} score={player.score} />;
+  });
+
   return (
     <>
       <h2>Game</h2>
-      {players.map((player) => {
-        return (
-          <Player key={player.id} name={player.name} score={player.score} />
-        );
-      })}
-      <Button background={"lightseagreen"}>end game</Button>
+      {!isPlaying && <p>Start a new game, enter your name below</p>}
+      {isPlaying && displayGame}
+      {isPlaying && (
+        <Button background={"lightseagreen"} onClick={endGameHandler}>
+          end game
+        </Button>
+      )}
       <AddPlayer />
     </>
   );
